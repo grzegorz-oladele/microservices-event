cca64a13f944d0951b8770475aa2b5380a974f51
grzesiek1362 <leslie.oladele@gmail.com>
2022-01-08 23:30:09 +0100

    creating Event Document

---
+++ event/src/main/java/pl/grzegorz/event/model/Event.java
@@ -0,0 +1,75 @@
+package pl.grzegorz.event.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
+public class Event {
+
+    @Id
+    private String code;
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long participantsLimit;
+    private Long participantsNumber;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Long getParticipantsLimit() {
+        return participantsLimit;
+    }
+
+    public void setParticipantsLimit(Long participantsLimit) {
+        this.participantsLimit = participantsLimit;
+    }
+
+    public Long getParticipantsNumber() {
+        return participantsNumber;
+    }
+
+    public void setParticipantsNumber(Long participantsNumber) {
+        this.participantsNumber = participantsNumber;
+    }
+}
